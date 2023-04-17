b055319891f7f8433e00d57139d5af4c55c0250e
garydavenport73 <software@garydavenport.com>
2023-04-17 12:16:43 -0400

    Added import export vcard contacts

--- contacts_javascript.js
+++ contacts_javascript.js
@@ -247,6 +247,175 @@ function saveOutlookContactsCSV(){
     saveStringToTextFile(thisCSV, contactsTable["name"] + getTodaysDate(), ".csv");
 }
 
+function importVCF() {
+    if (confirm("This will merge entries.\n\nIf you want to over-write all contents, please clear the contacts first, then proceed with loading the data.")) {
+        let fileContents = "";
+        let inputTypeIsFile = document.createElement('input');
+        inputTypeIsFile.type = "file";
+        inputTypeIsFile.accept = ".vcf";
+        inputTypeIsFile.addEventListener("change", function () {
+            let inputFile = inputTypeIsFile.files[0];
+            let fileReader = new FileReader();
+            fileReader.onload = function (fileLoadedEvent) {
+                fileContents = fileLoadedEvent.target.result;
+                readInAllVcards(fileContents);
+                contactsTableElement.innerHTML = buildContactsTableElement(contactsTable);
+                showMain("main-contacts-table");
+                //makeCalendar();
+            };
+            fileReader.readAsText(inputFile, "UTF-8");
+        });
+        inputTypeIsFile.click();
+    }
+}
 
+function readInAllVcards(contents) {
+    let startIndex = contents.indexOf("BEGIN:VCARD");
+    let endIndex = contents.indexOf("END:VCARD");
+    while (startIndex !== -1) {
+        let vCard = contents.substring(startIndex, endIndex + 9);
+        let contactObject = parseContactFromVCard(vCard);
+        contactsTable["data"].push(contactObject);
+        startIndex = contents.indexOf("BEGIN:VCARD", endIndex);
+        endIndex = contents.indexOf("END:VCARD", startIndex + 1);
+    };
+}
+
+function parseContactFromVCard(vCard) {
+    vCard = vCard.trim();
+    vCard = vCard.replace(/;PREF/g, "");
+    vCard = vCard.replace(/;VOICE/g, ";HOME");
+    vCard = vCard.replace(/\r\n|\n|\r/gm, "\n");
+    let lines = vCard.split("\n");
+    let prefixes = ["ADR;HOME:", "EMAIL:", "FN:", "NOTE:", "TEL;CELL:", "TEL;HOME:", "TEL;WORK:", "N:"];
+    let contactObjectKeys = ["Home Address", "E-mail Address", "First Name", "Notes", "Mobile Phone", "Home Phone", "Business Phone", "Last Name", "Middle Name"];
+    let contactObject = {};
+    for (let i = 0; i < prefixes.length; i++) {
+        for (j = 0; j < lines.length; j++) {
+            if (lines[j].substring(0, prefixes[i].length) === prefixes[i]) {
+                if (prefixes[i] === "N:") {
+                    let lastName = lines[j].substring(prefixes[i].length).split(";")[0];
+                    //console.log("Last Name is " + lastName);
+                    //if ((lastName!==undefined)&&(lastName!=="")){
+                    contactObject["Last Name"] = lastName;
+                    //}
+                    let middleName = lines[j].substring(prefixes[i].length).split(";")[2];
+                    //console.log("Middle Name is " + middleName);
+                    //if ((middleName!==undefined)&&(middleName!=="")){
+                    contactObject["Middle Name"] = middleName;
+                    //}
+                    let firstName = lines[j].substring(prefixes[i].length).split(";")[1];
+                    //console.log("Middle Name is " + middleName); 
+                    //if ((firstName!==undefined)&&(firstName!=="")){
+                    contactObject["First Name"] = firstName;
+                    //}
+                }
+                else {
+                    console.log("found match for ", prefixes[i],);
+                    // if (prefixes[i]==="FN:"){// only use first name from FN if it has not been defined from N
+                    //     if ((contactObject["First Name"]=== undefined)||(contactObject["First Name"].trim()=== "")){
+                    //         contactObject["First Name"] = lines[j].substring(3);//start after "FN:"
+                    //     }
+                    // }
+                    // else
+                    {
+                        contactObject[contactObjectKeys[i]] = lines[j].substring(prefixes[i].length);
+                    }
+                }
+            }
+        }
+    }
+    //if N data is present, convert it to first name
+    //
+
+
+    let headers = contactsTable["headers"];
+    for (let i = 0; i < headers.length; i++) {
+        if (contactObject.hasOwnProperty(headers[i])) {
+            //do nothing
+        }
+        else {
+            contactObject[headers[i]] = "";
+        }
+    }
+
+
+    let extras = ["ADR", "AGENT", "ANNIVERSARY", "BDAY", "CALADRURI", "CALURI", "CATEGORIES", "CLASS", "CLIENTPIDMAP", "FBURL", "GENDER", "GEO", "IMPP", "KEY", "KIND", "LABEL", "LANG", "MAILER", "MEMBER", "NAME", "NICKNAME", "ORG", "PRODID", "PROFILE", "RELATED", "REV", "ROLE", "SORT-STRING", "SOURCE", "TEL", "TITLE", "TZ", "UID", "URL", "XML"];
+    for (let i = 0; i < lines.length; i++) {
+        if (prefixes.includes(lines[i].split(":")[0] + ":")) {
+            //do nothing, move to next line
+        }
+        else {
+            for (j = 0; j < extras.length; j++) {
+                if (lines[i].substring(0, extras[j].length) === extras[j]) {
+                    console.log("found extra ", extras[j]);
+                    contactObject["Notes"] += "\n" + extras[j] + lines[i].substring(extras[j].length);
+                }
+            }
+        }
+    }
+    return contactObject;
+}
+
+//All vCards begin with 
+//BEGIN:VCARD
+//VERSION:
+//END:VCARD. All vCards must contain the VERSION property, which specifies the vCard version. VERSION must come immediately after BEGIN, except in the vCard 2.1 standard, which allows it to be anywhere in the vCard. Otherwise, properties can be defined in any order.
+function exportVCF() {
+    let str = "";
+    let rows = contactsTable["data"];
+    for (let i = 0; i < rows.length; i++) {
+        console.log(rows[i]);
+        str += "BEGIN:VCARD\n";
+        str += "VERSION:2.1\n";
+        //let prefixes = ["ADR;HOME:", "EMAIL:", "FN:", "NOTE:", "TEL;CELL:", "TEL;HOME:", "TEL;WORK:", "N:"];
+        //let contactObjectKeys = ["Home Address", "E-mail Address", "First Name", "Notes", "Mobile Phone", "Home Phone", "Business Phone", "Last Name", "Middle Name"];
+        let familyName = "";
+        let givenName = "";
+        let middleName = "";
+        if (rows[i]["Last Name"] !== undefined) {
+            familyName = rows[i]["Last Name"];
+            familyName = familyName.replace(/\;/g, " ");//remove ; because will throw off format, prevents user injection of ;
+        }
+        if (rows[i]["First Name"] !== undefined) {
+            givenName = rows[i]["First Name"];
+            givenName = givenName.replace(/\;/g, " ");
+        }
+        if (rows[i]["Middle Name"] !== undefined) {
+            middleName = rows[i]["Middle Name"];
+            middleName = middleName.replace(/\;/g, " ");
+        }
+        str += "N:" + familyName + ";" + givenName + ";" + middleName + ";;\n"
+        //if (givenName.trim()!==""){
+        str += "FN:" + givenName + " " + middleName + " " + familyName + "\n";
+        //}
+
+        if ((rows[i]["Mobile Phone"] !== undefined) && (rows[i]["Mobile Phone"] !== "")) {
+            str += "TEL;CELL:" + rows[i]["Mobile Phone"] + "\n";
+        }
+        if ((rows[i]["Home Phone"] !== undefined) && (rows[i]["Home Phone"] !== "")) {
+            str += "TEL;HOME:" + rows[i]["Home Phone"] + "\n";
+        }
+        if ((rows[i]["Business Phone"] !== undefined) && (rows[i]["Business Phone"] !== "")) {
+            str += "TEL;WORK:" + rows[i]["Business Phone"] + "\n";
+        }
+        if ((rows[i]["E-mail Address"] !== undefined) && (rows[i]["E-mail Address"] !== "")) {
+            str += "EMAIL:" + rows[i]["E-mail Address"] + "\n";
+        }
+        if ((rows[i]["Home Address"] !== undefined) && (rows[i]["Home Address"] !== "")) {
+            str += "ADR;HOME:" + rows[i]["Home Address"] + "\n";
+        }
+        if ((rows[i]["Notes"] !== undefined) && (rows[i]["Notes"] !== "")) {
+            str += "NOTE:" + rows[i]["Notes"] + "\n";
+        }
+        str += "END:VCARD\n";
+    }
+
+    console.log(str);
+    copyToClipBoard(str);
+    if (confirm(".vcf contents copied to clipboard.  Also save to file?")) {
+        saveStringToTextFile(str, "vcf" + contactsTable["name"] + getTodaysDate(), ".vcf");
+    }
+}
 
 
