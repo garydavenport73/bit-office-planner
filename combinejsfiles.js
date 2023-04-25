0c953c284a9098572e0c984214246612799a625b
garydavenport73 <software@garydavenport.com>
2023-04-25 12:13:37 -0400

    removed non-planner stuff

--- combinejsfiles.js
+++
@@ -1,9 +0,0 @@
-const fs=require('fs');
-let str="";
-str+=fs.readFileSync("globalVariables.js","utf-8");
-str+=fs.readFileSync("csvreaderwriter.js","utf-8");
-str+=fs.readFileSync("commonFunctions.js","utf-8");
-str+=fs.readFileSync("planner.js","utf-8");
-str+=fs.readFileSync("nestedmenu.js","utf8");
-fs.writeFileSync("javascript.js",str,"utf8");
-
